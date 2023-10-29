eaeb44c983a5169ada087c96829500f9e0e07865
Niels Rotmensen <git@nierot.com>
2023-10-29 15:00:19 +0100

    bug: Remove hourly data for articles

--- scraper.py
+++ scraper.py
@@ -7,15 +7,17 @@ config.fetch_images = False
 
 '''Gets all articles regarding the keyword, limit the search with the date options, by default the last year. Retursn a dict with date as key, and an array of results as value.'''
 def get_articles(keyword, start=None, end=None):
-    print("Getting articles ...")
+    print("Getting articles...")
     # Initialize the google news package
     google_news = GNews(language='en')
+    
+    google_news.max_results = 1000
 
     if start == None or end == None:
         google_news.period = '1m'
     else:
-        google_news.start_date(start)
-        google_news.end_date(end)
+        google_news.start_date = start
+        google_news.end_date = end
 
     print("Start querying GNews")
     news_result = google_news.get_news(keyword)
@@ -24,6 +26,9 @@ def get_articles(keyword, start=None, end=None):
     for article in news_result:
         date = article['published date']
         date = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %Z")
+        
+        # Remove hours, minutes etc.
+        date = date.replace(hour=0, minute=0, second=0, microsecond=0)
 
         if date not in dateNewsMap:
             dateNewsMap[date] = []
@@ -36,6 +41,6 @@ def get_articles(keyword, start=None, end=None):
             dateNewsMap[date].append(article3k.text)
         except:
             # Ignore errors
-            1+1
+            pass
 
     return dateNewsMap
